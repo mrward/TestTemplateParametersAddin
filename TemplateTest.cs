2e81cf0f8c8a45439a600b88c64d239d85e43326
Matt Ward <ward.matt@gmail.com>
2015-01-29 15:33:09 +0000

    Move file with conditional code out of project template.

---
+++ TemplateTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+$if$ ($AddSystemXml$ == true)using System.Xml;
+using System.Xml.Linq;
+$else$using System.Linq;
+$endif$using System.Text;
+
+namespace ${Namespace}
+{
+	public class ${Name}
+	{
+		public ${Name} ()
+		{
+		}
+		$if$ ($AddToStringMethod$ == true)
+			public override string ToString ()
+		{
+			return "${Name}";
+		}
+		$endif$	}
+}
